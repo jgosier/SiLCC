2c05a6948aa0da5f03b351d3d31d2ba48df6283e
Victor Miclovich <vicmiclovich@gmail.com>
2010-02-27 00:27:03 +0300

    added time print capability

--- sil/jsonrest.py
+++ sil/jsonrest.py
@@ -13,17 +13,25 @@ class Tweet():
 
 def search_twitter():
 	search_url = 'http://search.twitter.com/search.json?q=Django'
-	raw = urllib.urlopne(search_url)
+
+	raw = urllib.urlopen(search_url)
+	print "Read off search URL..."
 	js = raw.readlines()
+	print "transferred all data to js"
 	js_object = json.loads(js[0])
-	
+	print "loading json object complete"
 	tweets = []
 	for item in js_object['results']:
 		user = item['from_user']
 		graphic = item['profile_image_url']
 		text = item['text']
-		time = item['created_at']
-		
-		thistweet = Tweet(urser,text,graphic,time)
+		time = item['created_at']		
+		thistweet = Tweet(user,text,graphic,time)
 		tweets.append(thistweet)
 	return tweets
+
+x = search_twitter()
+for i in x:
+	print i.text
+	print i.graphic
+	print i.time
