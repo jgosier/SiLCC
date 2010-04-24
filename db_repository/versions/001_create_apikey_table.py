cd0f070ab38b76536226714b3223b904e4d633c3
Neville Newey <nn@theneweys.org>
2010-04-24 06:51:26 -0700

    Added call count and last called columns to apikey table

--- db_repository/versions/001_create_apikey_table.py
+++ db_repository/versions/001_create_apikey_table.py
@@ -15,7 +15,9 @@ apikey_table = Table('apikey', metadata,
     Column('owner_url', VARCHAR(128)),
     Column('valid_from', DATETIME, default=datetime.datetime.now()),
     Column('valid_to', DATETIME, server_default='2020-01-01'),                 
-    Column('comment', VARCHAR(256))                                                                     
+    Column('comment', VARCHAR(256)),
+    Column('calls', mysql.MSBigInteger(unsigned=True), server_default='0'),
+    Column('last_call', DATETIME)                 
     # Note: SQLAlchemy doesnt seem to have a way to create a current_timestamp col
     # So see upgrade script 2 where we do it with raw sql.
     #Column('created', TIMESTAMP, default='current_timestamp')
