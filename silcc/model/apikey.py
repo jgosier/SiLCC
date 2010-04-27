edee81e7caa4b67ec6062d42e5edbb14882d2e71
Neville Newey <nn@theneweys.org>
2010-04-26 17:03:26 -0700

    Added calls relation to APIKey class

--- silcc/model/apikey.py
+++ silcc/model/apikey.py
@@ -8,12 +8,12 @@ from sqlalchemy import and_, desc, func, or_, select, types
 from sqlalchemy.sql import text
 from sqlalchemy.orm import mapper, relation
 
-from silcc.model import meta
+from silcc.model import meta, APICall
 
 log = logging.getLogger(__name__)
 
 
 class APIKey(object):
+    calls = relation(APICall, backref="apikey")
 
-    pass
 
