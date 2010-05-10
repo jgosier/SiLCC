6c55154b9efc698d9dc066597841c504264d6c48
Neville Newey <nn@theneweys.org>
2010-05-10 16:21:26 -0700

    Added client Web API Python binding (silcc/client.py)

---
+++ silcc/client.py
@@ -0,0 +1,21 @@
+import urllib2
+import urllib
+
+import simplejson
+
+def tag(text):
+    if type(text) == unicode:
+        text = text.encode('utf-8')
+    values = dict(text=text,key='AAAABBBB')
+    data = urllib.urlencode(values)
+    request = urllib2.Request('http://opensilcc.com/api/tag', data)
+    json_response = urllib2.urlopen(request)
+    json_data = json_response.read()
+    json_response.close()
+    tags = simplejson.loads(json_data)
+    return tags
+
+if __name__ == '__main__':
+    tags = tag("Ushahidi is a platform for tracking crisis data")
+    print tags
+
