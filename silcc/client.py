98b755a3cc82d33702a6512afbbf918ad4a039ab
Neville Newey <nn@theneweys.org>
2010-05-11 08:16:39 -0700

    Added docstrings to Python client bindings

--- silcc/client.py
+++ silcc/client.py
@@ -1,19 +1,21 @@
+"""Python Bindings for SiLCC tagging API"""
 import urllib2
 import urllib
 
 import simplejson
 
 def tag(text):
+    """Calls the SilCC web service API"""
     if type(text) == unicode:
         text = text.encode('utf-8')
-    values = dict(text=text,key='AAAABBBB')
+    values = dict(text=text, key='AAAABBBB')
     data = urllib.urlencode(values)
     request = urllib2.Request('http://opensilcc.com/api/tag', data)
     json_response = urllib2.urlopen(request)
     json_data = json_response.read()
     json_response.close()
-    tags = simplejson.loads(json_data)
-    return tags
+    retval = simplejson.loads(json_data)
+    return retval
 
 if __name__ == '__main__':
     tags = tag("Ushahidi is a platform for tracking crisis data")
