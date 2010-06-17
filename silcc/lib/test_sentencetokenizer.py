f636ddb6ae6ddab9a814a0b8e675a1b7c6db694b
Nishith Rastogi <mail@nishithrastogi.com>
2010-06-17 23:40:29 +0530

    Updated test_sentencetokenizer.py to account for new tokens, currently all 4 nosetest pass

--- silcc/lib/test_sentencetokenizer.py
+++ silcc/lib/test_sentencetokenizer.py
@@ -3,14 +3,15 @@ import csv
 from silcc.lib.sentencetokenizer import SentenceTokenizer
 
 def test_sentencetokenizer():
+
     tokens = SentenceTokenizer.tokenize("This is a sentence. And this is another.")
-    assert tokens == [
-        ('CAPITILIZED', 'This'), ('LOWER', 'is'), ('LOWER', 'a'), 
-        ('LOWER', 'sentence'), ('TERMINATOR', '.'), ('CAPITILIZED', 'And'), 
-        ('LOWER', 'this'), ('LOWER', 'is'), ('LOWER', 'another'), 
-        ('TERMINATOR', '.')]
+    assert tokens == [('FIRST_CAPITALIZED_STOPWORD', 'This'), ('LOWER_STOPWORD', 'is'),
+		     ('LOWER_STOPWORD', 'a'), ('LOWER', 'sentence'), ('TERMINATOR', '.'),
+                     ('CAPITALIZED_STOPWORD', 'And'), ('LOWER_STOPWORD', 'this'),
+                     ('LOWER_STOPWORD', 'is'), ('LOWER', 'another'), ('TERMINATOR', '.')]
+
     tokens = SentenceTokenizer.tokenize("This Is A Sentence Of Type Allcaps.")
-    assert tokens == [
-        ('CAPITILIZED', 'This'), ('CAPITILIZED', 'Is'), ('MIXED', 'A'), 
-        ('CAPITILIZED', 'Sentence'), ('CAPITILIZED', 'Of'), 
-        ('CAPITILIZED', 'Type'), ('CAPITILIZED', 'Allcaps'), ('TERMINATOR', '.')]
+    assert tokens == [('FIRST_CAPITALIZED_STOPWORD', 'This'), ('CAPITALIZED_STOPWORD', 'Is'),
+		     ('MIXED_STOPWORD', 'A'), ('CAPITALIZED', 'Sentence'), 
+            	     ('CAPITALIZED_STOPWORD', 'Of'), ('CAPITALIZED', 'Type'),
+		     ('CAPITALIZED', 'Allcaps'), ('TERMINATOR', '.')]
