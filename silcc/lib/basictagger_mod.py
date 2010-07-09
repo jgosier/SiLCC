b2ee045de99c45eb2d600876f0af54225ec9bda0
Nishith Rastogi <mail@nishithrastogi.com>
2010-07-10 01:34:12 +0530

    added basictagger_mod.py, now texts like 'Linux is a good example of free software' gets handeled better

--- silcc/lib/basictagger_mod.py
+++ silcc/lib/basictagger_mod.py
@@ -20,6 +20,7 @@ log = logging.getLogger(__name__)
 # VBD - verb 
 
 pos_include = ('NN', 'NNP', 'NNS')
+pos_adj = ('JJ',  'JJR')
 
                  
 class BasicTagger(object):
@@ -32,18 +33,32 @@ class BasicTagger(object):
             return []
 
         text = text.replace("'", "")
-        
-        #Call to Capitalization Normalizer
         bt = BasicTokenizer()
-        n = Normalizer()
         
+        #Call to Capitalization Normalizer.
+        n = Normalizer()
         text = n.normalizer(text)
+        
         tokens = bt.tokenize(text)
         pos = nltk.pos_tag(tokens)
+        for a in pos:
+            print a  
         
         
         # Only return those tokens whose pos is in the include list
-        tags = [t[0] for t in pos if t[1] in pos_include]
+        #tags = [t[0] for t in pos if t[1] in pos_include]
+        
+        tags = []
+        
+        for i, t in enumerate(pos):
+            if ((pos[i-1][1] in pos_adj) and (pos[i][1] in pos_include)):
+                temp = pos[i-1][0] + " " + pos[i][0]
+                tags.append(temp)
+            elif(pos[i][1] in pos_include):
+                tags.append(pos[i][0])
+    
+        print pos[i][0] + " " + pos[i][1]
+            
 
         # Now exclude stopwords...
         tags = [t for t in tags if not t in stop_words]
@@ -56,6 +71,7 @@ class BasicTagger(object):
         # We will also preserve uppercased tags if they are the first occurence
 
         tags_ = CIList()
+        print tags
         for t in tags:
             if t in tags_: 
                 continue
